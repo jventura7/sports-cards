ea902fbe651f1204903f5134128bac57d98fd14d
jventura7 <82546770+jventura7@users.noreply.github.com>
2021-11-09 15:50:43 -0500

    Updated hough transform to detect rotation

--- HoughTransform.py
+++ HoughTransform.py
@@ -1,64 +1,73 @@
 import cv2
 import numpy as np
+from skimage.transform import hough_line, hough_line_peaks
+from matplotlib import cm
+import matplotlib.pyplot as plt
 
-img = cv2.imread('baseball_card7.png', 0)
+# Read image and perform Canny Edge Detection
+img = cv2.imread('baseball_card.jpg', 0)
 black_img = np.zeros(img.shape)
-edges = cv2.Canny(img, 100, 150)
-lines = cv2.HoughLines(edges, rho=1, theta=np.pi/180, threshold=60)
-N = lines.shape[0]
+edges = cv2.Canny(img, 100, 120)
+
+# Obtain Hough Lines
+h, theta, d = hough_line(edges)
+fig, ax = plt.subplots()
+ax.imshow(img, cmap=cm.gray)
+row1, col1 = img.shape
+origin = np.array((0, img.shape[1]))
 
 horizontal_lines = []
 vertical_lines = []
-for i in range(N):
-    rho = lines[i][0][0]
-    theta = lines[i][0][1]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 1000 * (-b))
-    y1 = int(y0 + 1000 * a)
-    x2 = int(x0 - 1000 * (-b))
-    y2 = int(y0 - 1000 * a)
-    if y1 == y2 or abs(y1 - y2) <= 20:
-        horizontal_lines.append(lines[i])
-    if x1 == x2 or abs(x1 - x2) <= 20:
-        vertical_lines.append(lines[i])
-    # cv2.line(black_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-vertical_lines.sort(key=lambda x: x[0][0])
+for _, angle, dist in zip(*hough_line_peaks(h, theta, d, threshold=0.5*h.max())):
+    y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
+    print(y0, y1)
+    if (y0 == y1 or abs(y0 - y1) <= 20) and y0 <= 600 and y1 <= 600:
+        horizontal_lines.append([y0, y1])
+    if abs(y0) >= 10000 and abs(y1) >= 10000:
+        vertical_lines.append([y0, y1])
+    # ax.plot(origin, (y0, y1), '-r')
+# ax.axis((0, col1, row1, 0))
+# ax.set_title('Detected lines')
+# ax.set_axis_off()
+
+# Sort Hough Lines to get card borders
+for i in range(len(vertical_lines)):
+    for j in range(len(vertical_lines[i])):
+        vertical_lines[i] = [abs(vertical_lines[i][0]), vertical_lines[i][1]]
+        break
+vertical_lines.sort(key=lambda x: x[0])
 vertical_lines = vertical_lines[:2] + vertical_lines[-2:]
+print(vertical_lines)
+length_left = vertical_lines[1][0] - vertical_lines[0][0]
+length_right = vertical_lines[3][0] - vertical_lines[2][0]
+total_left_right = length_left + length_right
+print("Length of margins (pixels): Left: {left}  Right: {right}".format(left=int(length_left), right=int(length_right)))
+print("Left to Right Ratio: {left_ratio}:{right_ratio}".format(left_ratio=int(round(length_left / total_left_right * 100)),
+                                                               right_ratio=int(round(length_right / total_left_right * 100))))
 
-length_bottom = vertical_lines[1][0][0] - vertical_lines[0][0][0]
-length_top = vertical_lines[3][0][0] - vertical_lines[2][0][0]
-print(length_bottom, length_top)
-print("Bottom to Top Ratio ", length_bottom / length_top)
-
-horizontal_lines.sort(key=lambda x: x[0][0])
+horizontal_lines.sort(key=lambda x: x[0])
 horizontal_lines = horizontal_lines[:2] + horizontal_lines[-2:]
 
-length_left = horizontal_lines[1][0][0] - horizontal_lines[0][0][0]
-length_right = horizontal_lines[3][0][0] - horizontal_lines[2][0][0]
-print(length_right, length_left)
-print("Left to Right Ratio ", length_left / length_right)
-
+length_bottom = horizontal_lines[1][0] - horizontal_lines[0][0]
+length_top = horizontal_lines[3][0] - horizontal_lines[2][0]
+total_bottom_top = length_bottom + length_top
+print("Length of margins (pixels): Bottom: {bottom}  Top: {top}".format(bottom=int(length_bottom), top=int(length_top)))
+print("Bottom to Top Ratio: {bottom_ratio}:{top_ratio}".format(bottom_ratio=int(round(length_bottom / total_bottom_top * 100)),
+                                                               top_ratio=int(round(length_top / total_bottom_top * 100))))
+# Plot card borders
 borders = horizontal_lines + vertical_lines
-for line in borders:
-    rho = line[0][0]
-    theta = line[0][1]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 1000 * (-b))
-    y1 = int(y0 + 1000 * a)
-    x2 = int(x0 - 1000 * (-b))
-    y2 = int(y0 - 1000 * a)
-    cv2.line(black_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+for border in borders:
+    ax.plot(origin, (border[0], border[1]), '-r')
+ax.axis((0, col1, row1, 0))
+ax.set_title('Detected lines')
+ax.set_axis_off()
+fig.savefig('houghlines.png')
 
 img = img.astype(np.float32) / 255
 stack = np.hstack((img, black_img))
 cv2.imshow('Hough Lines', stack)
+cv2.imshow('edges', edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 cv2.waitKey(1)
